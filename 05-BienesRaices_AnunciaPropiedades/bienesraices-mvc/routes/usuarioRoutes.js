6fa3094f240fa3b487ed578291c4c1906ff756c3
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-10 12:34:40 -0400

    Probando Peticiones GET y POST

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/routes/usuarioRoutes.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/routes/usuarioRoutes.js
@@ -2,14 +2,29 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send('Hola mundo desde Express!');
-    // res.json({msg: 'Hola mundo desde Express!'});
-    // res.render({msg: 'Hola mundo desde Express!'});
+// router.get('/', function (req, res) {
+//     res.json({msg: 'Hola mundo desde Express!'});
+// });
+
+// router.post('/', function (req, res) {
+//     res.json({msg: 'Respuesta de tipo POST'});
+// });
+
+router.get('/', (req, res) => {
+    res.json({msg: 'Hola mundo desde Express!'});
 });
 
-router.get('/nosotros', function (req, res) {
-    res.send('Desde pagina de Nosotros');
+router.post('/', (req, res) => {
+    res.json({msg: 'Respuesta de tipo POST'});
 });
+
+// Otra forma
+// router.route('/')
+//     .get(function (req, res) {
+//         res.json({msg: 'Hola mundo desde Express!'});
+//     })
+//     .post(function (req, res) {
+//         res.json({msg: 'Respuesta de tipo POST'});
+//     });
 
 export default router;
