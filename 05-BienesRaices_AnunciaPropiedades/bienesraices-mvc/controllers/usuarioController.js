9b806f9a98549a59be60c6fd99a638d09ac1e3d8
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-16 18:30:13 -0400

    Agregando el resto de Validaciones

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/controllers/usuarioController.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/controllers/usuarioController.js
@@ -20,8 +20,14 @@ const registrar = async (req, res) => {
 
     // Validación
     await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req);
+    await check('email').isEmail().withMessage('El email no es valido').run(req);
+    // await check('email').notEmpty().withMessage('El email es obligatorio').isEmail().withMessage('El email no es valido').run(req);
+    await check('password').isLength({min: 6}).withMessage('El password debe contener al menos 6 caracteres').run(req);
+    await check('repetir_password').equals('password').withMessage('Los passwords no son iguales').run(req);
 
     let resultado = validationResult(req);
+
+    // Verificar que el resultado este vacio
 
     res.json(resultado.array());
 
