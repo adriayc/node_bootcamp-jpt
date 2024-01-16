656bb7d090be8aeb42e3b012271ab1003e9bff32
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-15 23:43:18 -0400

    Añadiendo los campos necesarios para el Modelo

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/models/Usuario.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/models/Usuario.js
@@ -4,5 +4,21 @@ import db from '../config/db.js';
 
 // Definimos el nombre de la tabla y sus atributos
 const Usuario = db.define('usuarios', {
-    
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token: DataTypes.STRING,
+    // token: { type: DataTypes.STRING }
+    confirmado: DataTypes.BOOLEAN
 });
+
+export default Usuario;
