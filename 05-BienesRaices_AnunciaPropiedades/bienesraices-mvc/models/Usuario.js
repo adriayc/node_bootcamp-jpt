47eeeb5eb8e9cae4573a74a47342f7c319acdb3a
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-15 23:36:06 -0400

    Primeros pasos definiendo un Modelo

---
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/models/Usuario.js
@@ -0,0 +1,8 @@
+import { DataTypes } from 'sequelize';
+// DBs
+import db from '../config/db.js';
+
+// Definimos el nombre de la tabla y sus atributos
+const Usuario = db.define('usuarios', {
+    
+});
