e15782601a8a3b2613e81af2d48269b9f7589f25
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-15 23:50:21 -0400

    Comunicando la Vista con el Routing y el Controller

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/routes/usuarioRoutes.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/routes/usuarioRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 // Controllers
-import { formularioLogin, formularioRegistro, formularioOlvidePassword } from '../controllers/usuarioController.js';
+import { formularioLogin, formularioRegistro, registrar, formularioOlvidePassword } from '../controllers/usuarioController.js';
 
 const router = express.Router();
 
 router.get('/login', formularioLogin);
+
 router.get('/registro', formularioRegistro);
+router.post('/registro', registrar);
+
 router.get('/olvide-password', formularioOlvidePassword);
 
 export default router;
