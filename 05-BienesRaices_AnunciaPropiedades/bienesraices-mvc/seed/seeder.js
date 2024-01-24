9acca6ebb4cb7dd0619f151e1c8640b4a15e4b87
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-23 22:36:02 -0400

    Primeros pasos creando un Seeder para la Base de Datos

---
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/seed/seeder.js
@@ -0,0 +1,30 @@
+import { exit } from 'node:process';
+// Seeds
+import categorias from './categorias.js';
+// Models
+import Categoria from '../models/Categoria.js';
+// Db's
+import db from '../config/db.js';
+
+const importarDatos = async () => {
+    try {
+        // Autenticar
+        await db.authenticate();
+
+        // Generar las columnas
+        await db.sync();
+
+        // Insertar los datos
+        await Categoria.bulkCreate(categorias);
+        console.log('Datos importados correctamente');
+        exit();
+        // exit(0);        // 0 - Finalizo correctamente
+        
+    } catch (error) {
+        console.log(error);
+        // Finalizar el proceso
+        // process.exit(1);
+        // Solo node.js
+        exit(1);        // 1 - Finalizo incorrectamente
+    }
+};
