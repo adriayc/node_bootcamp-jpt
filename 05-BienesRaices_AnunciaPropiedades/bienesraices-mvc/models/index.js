a33780537cf22b6c6cc6df03a5a5daed04fdc60f
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-24 18:46:27 -0400

    Solucion Reto 02 - Agregar la asociacion de la tabla Propiedades con Categoria y Usuario

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/models/index.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/models/index.js
@@ -7,7 +7,13 @@ import Precio from './Precio.js';
 // Relacion 1:1
 // Una Propiedad tiene un Precio
 // Precio.hasOne(Propiedad);
-Propiedad.belongsTo(Precio, {foreignKey: 'fk_precioId'});
+Propiedad.belongsTo(Precio, {foreignKey: 'precioId'});
+// Una Propiedad tiene una Categoria
+// Categoria.hasOne(Propiedad);
+Propiedad.belongsTo(Categoria,  {foreignKey: 'categoriaId'});
+// Una Propiedad tiene un Usuario
+// Usuario.hasOne(Propiedad);
+Propiedad.belongsTo(Usuario, {foreignKey: 'usuarioId'})
 
 export {
     Usuario,
