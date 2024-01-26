114556f375c8f2a467e7926648502ca44800c16c
Adriano Ayala <adriano.ayc@gmail.com>
2024-01-26 10:37:21 -0400

    Leyendo y Validando JWT

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/middleware/protegerRuta.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/middleware/protegerRuta.js
@@ -1,9 +1,30 @@
-const protegerRuta = (req, res, next) => {
+// Json Web Token
+import jwt from 'jsonwebtoken';
+// Models
+import { Usuario } from '../models/index.js';
+
+const protegerRuta = async (req, res, next) => {
     // console.log('Desde el middleware...');
 
     // Verificar si hay un token
+    // console.log(req.cookies._token);
+    const { _token } = req.cookies;
+    if (!_token) {
+        return res.redirect('/auth/login');
+    }
 
     // Comprobar el token
+    try {
+        // Verificar el JWT
+        const decoded = jwt.verify(_token, process.env.JWT_SECRET);
+        // console.log(decoded);
+
+        const usuario = await Usuario.findByPk(decoded.id);
+        // console.log(usuario);
+        
+    } catch (error) {
+        return res.clearCookie('_token').redirect('/auth/login');
+    }
 
     next();
 };
