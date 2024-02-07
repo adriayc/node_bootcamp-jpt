d8628ac48ee9d12b47f1fe8e16e2b5a8830d3b2b
Adriano Ayala <adriano.ayc@gmail.com>
2024-02-07 12:14:53 -0400

    Finalizando el JSON de Propiedades

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/controllers/apiController.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/controllers/apiController.js
@@ -1,7 +1,16 @@
+// Models
+import { Propiedad, Categoria, Precio } from '../models/index.js';
+
 const propiedades = async (req, res) => {
-    res.json({
-        respuesta: 'Ok'
+    // Obtener todas la propiedad (si existe muchos registros limitar los ultimos para evitar la latencia)
+    const propiedades = await Propiedad.findAll({
+        include: [
+            {model: Categoria, as: 'categoria'},
+            {model: Precio, as: 'precio'}
+        ]
     });
+
+    res.json(propiedades);
 };
 
 export {
