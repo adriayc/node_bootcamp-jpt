c428e5ca2a0913e3cf6e4a5683ec3e1e81ded724
Adriano Ayala <adriano.ayc@gmail.com>
2024-02-07 12:29:58 -0400

    Consultando el JSON de Propiedades

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/src/js/mapaInicio.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/src/js/mapaInicio.js
@@ -8,4 +8,23 @@
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    // Obtener propiedades en formato JSON
+    const obtenerPropiedades = async () => {
+        try {
+            // URL de la peticion (no requiere http://localhost:3000 o https://dominio por que se encuentra en el mismo host)
+            const url = '/api/propiedades';
+            const respuesta = await fetch(url);
+            const propiedades = await respuesta.json();
+            // console.log(respuesta);
+            console.log(propiedades);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // Llamar la funcion para obtener propiedades
+    obtenerPropiedades();
+
 })();
