9a59d68c5eafa6235ed8a1e82ff63aadbe2db6a4
Adriano Ayala <adriano.ayc@gmail.com>
2024-02-09 23:36:12 -0400

    Si el Usuario esta Autenticado puede enviar un mensaje

--- 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/middleware/identificarUsuario.js
+++ 05-BienesRaices_AnunciaPropiedades/bienesraices-mvc/middleware/identificarUsuario.js
@@ -4,15 +4,28 @@ import { Usuario } from '../models/index.js';
 
 const identificarUsuario = async (req, res, next) => {
     // Identificar si hay token
-    const token = req.cookies._token;
+    const { _token } = req.cookies;
 
-    if (!token) {
+    if (!_token) {
         req.usuario = null;
 
         return next();
     }
 
     // Comprobar el token
+    try {
+        const decoded = jwt.verify(_token, process.env.JWT_SECRET);
+        const usuario = await Usuario.scope('eliminarPassword').findByPk(decoded.id);
+
+        if (usuario) {
+            req.usuario = usuario;
+        }
+        return next();
+        
+    } catch (error) {
+        console.log(error);
+        return res.clearCookie('_token').redirect('/auth/login');
+    }
 
     // next();
 };
