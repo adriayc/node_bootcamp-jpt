88d7e5fb3cc2d0b2c3c5bc79af477a9212a158e3
Adriano Ayala <adriano.ayc@gmail.com>
2024-02-27 16:09:24 -0400

    Habilitando Handlebars como Template Engine

--- 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/index.js
+++ 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const path =  require('path');              // Utilidad de node que permite obtener la URL
+// Handlebars
+const exphbs = require('express-handlebars');
 // Routes
 const router = require('./routes');
 
 const app = express();
+
+// Habilitar handlebars como 'view'
+app.engine('handlebars', 
+    // exphbs({         // Error!
+    exphbs.engine({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', (req, res) => {
 //     res.send('Hola Mundo NodeJS!');
