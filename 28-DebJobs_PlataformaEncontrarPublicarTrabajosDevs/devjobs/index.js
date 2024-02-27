4b28a195a9ddc02e5d8f1e1f98da87e93871bee8
Adriano Ayala <adriano.ayc@gmail.com>
2024-02-27 16:08:28 -0400

    Creando el Router

--- 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/index.js
+++ 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+// Routes
+const router = require('./routes');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('Hola Mundo NodeJS!');
-});
+// app.use('/', (req, res) => {
+//     res.send('Hola Mundo NodeJS!');
+// });
+app.use('/', router());
 
 // Puerto
 app.listen(5000);
