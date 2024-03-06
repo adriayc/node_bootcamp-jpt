19e8b2e6d8cf30d2250b99ee1689972e6256d155
Adriano Ayala <adriano.ayc@gmail.com>
2024-03-06 10:01:03 -0400

    Finalizando el Formulario para editar las vacantes

--- 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/helpers/handlebars.js
+++ 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/helpers/handlebars.js
@@ -1,10 +1,13 @@
 module.exports = {
     seleccionarSkills: (seleccionadas = [], opciones) => {
+        // console.log(seleccionadas);
+        console.log(opciones.fn());
+
         const skills = ['HTML5', 'CSS3', 'CSSGrid', 'Flexbox', 'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS', 'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP', 'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize', 'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress'];
 
         let html = '';
         skills.forEach(skill => {
-            html += `<li>${skill}</li>`;
+            html += `<li ${seleccionadas.includes(skill) ? 'class="activo"' : ''}>${skill}</li>`;
         })
 
         return opciones.fn().html = html;
