06ff47e82abeb3f90c26e1ebf8562ac6bcf3df94
Adriano Ayala <adriano.ayc@gmail.com>
2024-03-11 11:10:29 -0400

    Validando los Campos para crear Usuarios

--- 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/controllers/usuarioController.js
+++ 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/controllers/usuarioController.js
@@ -1,6 +1,23 @@
+// const mongoose = require('mongoose');
+// Models
+// const Usuario = mongoose.model('Usuarios');
+const Usuario = require('../models/Usuarios');
+
 exports.formCrearCuenta = (req, res) => {
     res.render('crear-cuenta', {
         nombrePagina: 'Crea tu cuenta en debJobs',
         tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta'
     })
+};
+
+exports.crearUsuario = async (req, res, next) => {
+    // console.log(req.body);
+    // Crear el usuario
+    const usuario = new Usuario(req.body);
+    // console.log(usuario);
+    const nuevoUsuario = await usuario.save();
+
+    // if (!nuevoUsuario) return next();
+
+    res.redirect('/iniciar-sesion');
 };
