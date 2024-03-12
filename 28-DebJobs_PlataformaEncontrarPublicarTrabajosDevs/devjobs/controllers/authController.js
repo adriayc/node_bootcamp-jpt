3dc3ff6abab232013115c79bac0ec10d9d546f1f
Adriano Ayala <adriano.ayc@gmail.com>
2024-03-12 16:19:53 -0400

    Configurando Passport y mostrando errores

--- 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/controllers/authController.js
+++ 28-DebJobs_PlataformaEncontrarPublicarTrabajosDevs/devjobs/controllers/authController.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 
 exports.autenticarUsuario = passport.authenticate('local', {
-    successRedirect: '/ok',
-    failureRedirect: '/mal'
+    // successRedirect: '/ok',
+    successRedirect: '/administracion',
+    // failureRedirect: '/mal'
+    failureRedirect: '/iniciar-sesion',
+    failureFlash: true,
+    badRequestMessage: 'Ambos campos son obligatorios'
 });
