d767b84f6a70ca059061bac5cbc0a00b3f93672b
Adriano Ayala <adriano.ayc@gmail.com>
2024-03-19 00:18:50 -0400

    Agregando el Routing

--- 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/index.js
+++ 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 // Configurar dotenv
 require('dotenv').config({path: 'variables.env'});
+// Routers
+const router = require('./routes');
 
 // Inicializar express
 const app = express();
 
+app.use('/', router());
+
+// Agregar el puerto
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta funcionado');
 });
