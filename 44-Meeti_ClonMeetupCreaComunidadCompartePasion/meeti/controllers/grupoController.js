9d5d541404be399a9bc1859f100b6fa4cf52048f
Adriano Ayala <adriano.ayc@gmail.com>
2024-03-23 12:13:13 -0400

    Almacenando Grupos en la Base de Datos

--- 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/controllers/grupoController.js
+++ 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/controllers/grupoController.js
@@ -1,5 +1,6 @@
 // Models
 const Categoria = require('../models/Categoria');
+const Grupo = require('../models/Grupo');
 
 exports.formNuevoGrupo = async (req, res) => {
     const categorias = await Categoria.findAll();
@@ -10,4 +11,26 @@ exports.formNuevoGrupo = async (req, res) => {
         nombrePagina: 'Crear un nuevo grupo',
         categorias
     });
+};
+
+// Guardar el grupo en la DB
+exports.crearGrupo = async (req, res) => {
+    const grupo = req.body;
+    // console.log(grupo); return;
+
+    try {
+        // Almacenar el DB
+        await Grupo.create(grupo);
+
+        req.flash('exito', 'Se ha creardo el grupo correctamente');
+        // Redireccionar
+        res.redirect('/administracion');
+
+    } catch (error) {
+        console.log(error);
+
+        req.flash('error', error);
+        // Redireccionar
+        res.redirect('/nuevo-grupo');
+    }
 };
