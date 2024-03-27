07086ee0d458d7371664fe7fa8f563d557f20aff
Adriano Ayala <adriano.ayc@gmail.com>
2024-03-27 09:22:22 -0400

    Consultando los Meeti's

--- 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/controllers/adminController.js
+++ 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/controllers/adminController.js
@@ -1,11 +1,22 @@
 // Models
 const Grupo = require('../models/Grupo');
+const Meeti = require('../models/Meeti');
 
 exports.panelAdministracion = async (req, res) => {
-    const grupos = await Grupo.findAll({where: {usuarioId: req.user.id}});
+    // const grupos = await Grupo.findAll({where: {usuarioId: req.user.id}});
+    // Usando promise
+    // const consultas = [];
+    // consultas.push(Grupo.findAll({where: {usuarioId: req.user.id}}));
+    // consultas.push(Meeti.findAll({where: {usuarioId: req.user.id}}));
+    // const [grupos, meetis] = await Promise.all(consultas);
+    const [grupos, meetis] = await Promise.all([
+        Grupo.findAll({where: {usuarioId: req.user.id}}),
+        Meeti.findAll({where: {usuarioId: req.user.id}})
+    ]);
 
     res.render('administracion', {
         nombrePagina: 'Panel de Administración',
-        grupos
+        grupos,
+        meetis
     });
 };
