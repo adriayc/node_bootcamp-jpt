c9062c14b3f2130eeddb50023fb688788c3cbb74
Adriano Ayala <adriano.ayc@gmail.com>
2024-03-30 22:31:07 -0400

    Finalizando las Consultas para el buscador

--- 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/controllers/frontend/busquedaControllerFE.js
+++ 44-Meeti_ClonMeetupCreaComunidadCompartePasion/meeti/controllers/frontend/busquedaControllerFE.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const momento = require('moment');
+const moment = require('moment');
 // Models
 const Meeti = require('../../models/Meeti');
 const Grupo = require('../../models/Grupo');
@@ -14,8 +14,43 @@ exports.resultadosBusqueda = async (req, res) => {
     // Leer datos de la URL
     const {categoria, titulo, ciudad, pais} = req.query;
 
+    // Validar categoria
+    let query;
+    if (categoria === '') {
+        query = '';
+    } else {
+        query = `
+            where: {
+                categoriaId: {[Op.eq]: ${categoria}}
+            }
+        `;
+    }
+
     // Filstrar los meeti's por los terminos de busqueda
     const meetis = await Meeti.findAll({
-        where: {}
+        include: [
+            {
+                model: Grupo,
+                // where: {
+                //     categoriaId: {[Op.eq]: categoria}
+                // }
+                query
+            },
+            {
+                attributes: ['id', 'nombre', 'imagen'],
+                model: Usuario,
+            }
+        ],
+        where: {
+            titulo: {[Op.iLike]: '%'+ titulo +'%'}, 
+            ciudad: {[Op.iLike]: '%'+ ciudad +'%'}, 
+            pais: {[Op.iLike]: '%'+ pais +'%'}
+        }
+    });
+
+    res.render('busqueda', {
+       nombrePagina: 'Resultados Búsquedas',
+       meetis,
+       moment
     });
 };
