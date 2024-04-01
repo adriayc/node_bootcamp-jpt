a7619b3adceb421e74d4ced0ddc0e3dd9745a1e3
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-01 18:43:53 -0400

    Definiendo el Modelo de Pedidos

---
+++ 69-RestApis_CreandoProyectoRestApis/restapis/models/Pedido.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+// Inicializar schema
+const Schema = mongoose.Schema;
+
+const pedidoSchema = new Schema({
+    // Realacion 1:1 (Un pedido tiene 1 ciente)
+    cliente: {
+        type: Schema.ObjectId,
+        ref: 'Clientes'
+    },
+    // Relacion 1:n (Un pedido tiene 1 o muchos productos)
+    productos: [{
+       producto: {
+        type: Schema.ObjectId,
+        ref: 'Productos'
+       },
+       cantidad: Number
+    }],
+    total: {
+        type: Number
+    }
+});
+
+// Exportar el modelo
+module.exports = mongoose.model('Pedidos', pedidoSchema);
