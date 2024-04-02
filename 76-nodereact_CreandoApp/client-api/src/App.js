dfbedeecdb5063adbe305896b4ef8713cf52a613
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-02 16:19:49 -0400

    Creando la Navegacion

--- 76-nodereact_CreandoApp/client-api/src/App.js
+++ 76-nodereact_CreandoApp/client-api/src/App.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 // Components
 import Header from './components/layout/Header';
+import Navegacion from './components/layout/Navegacion';
 
 function App() {
   return (
@@ -12,6 +13,14 @@ function App() {
     <Fragment>
       {/* Call component */}
       <Header />
+
+      <div className='contenedor grid contenido-principal'>
+        <Navegacion />
+
+        <main className='caja-contenido col-9'>
+          {/* TODO: Routing a los diferentes Componentes */}
+        </main>
+      </div>
     </Fragment>
   );
 }
