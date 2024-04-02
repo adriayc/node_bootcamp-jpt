8911e1323f3d4cd08b37be1c4c49b9675c8bc6ed
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-02 18:02:28 -0400

    Habilitando Cors y Consumir la API

--- 76-nodereact_CreandoApp/client-api/src/components/clientes/Clientes.js
+++ 76-nodereact_CreandoApp/client-api/src/components/clientes/Clientes.js
@@ -1,4 +1,21 @@
+import { useEffect } from 'react';
+// ClienteAxios
+import clienteAxios from '../../config/exios';
+
 const Clientes = () => {
+    // Query a la API
+    const consultarAPI = async () => {
+        // console.log('Consultado...');
+
+        const clientes = await clienteAxios.get('/clientes');
+        console.log(clientes);
+    };
+
+    // Hook useEfect, similar a componetdidmount y willmount
+    useEffect(() => {
+        consultarAPI();
+    }, []);             // [] - Ejecuta solo una vez
+
     return (
         <h2>Clientes</h2>
     );
