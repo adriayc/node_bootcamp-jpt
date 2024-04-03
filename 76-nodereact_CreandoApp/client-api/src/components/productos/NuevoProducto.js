cc06fc698e6336c3c59578418a0db2df3c542cbf
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-03 18:50:43 -0400

    Colocando la informacion en el State

--- 76-nodereact_CreandoApp/client-api/src/components/productos/NuevoProducto.js
+++ 76-nodereact_CreandoApp/client-api/src/components/productos/NuevoProducto.js
@@ -8,6 +8,26 @@ const NuevoProducto = () => {
   });
   const [archivo, guardarArchivo] = useState('');
 
+  // Leer los datos (nombre y precio) del formulario
+  const actualizarProductoState = e => {
+    // Guardar en el state producto
+    guardProducto({
+      // Copiar del state actual
+      ...producto,
+      [e.target.name]: e.target.value
+    });
+    // console.log(producto);
+  };
+
+  // Leer el dato imagen del formulario
+  const actualizarArchivoState = e => {
+    // console.log(e.target.files);
+    // console.log(e.target.files[0]);
+
+    // Guardar en el state archivo ()
+    guardarArchivo(e.target.files[0]);
+  };
+
   return (
     <Fragment>
       <h2>Nuevo Producto</h2>
@@ -17,17 +37,33 @@ const NuevoProducto = () => {
 
         <div className="campo">
           <label>Nombre:</label>
-          <input type="text" placeholder="Nombre Producto" name="nombre" />
+          <input 
+            type="text" 
+            placeholder="Nombre Producto" 
+            name="nombre" 
+            onChange={actualizarProductoState}
+          />
         </div>
 
         <div className="campo">
           <label>Precio:</label>
-          <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" />
+          <input 
+            type="number" 
+            name="precio" 
+            min="0.00" 
+            step="0.01" 
+            placeholder="Precio" 
+            onChange={actualizarProductoState}
+          />
         </div>
     
         <div className="campo">
           <label>Imagen:</label>
-          <input type="file"  name="imagen" />
+          <input 
+            type="file"  
+            name="imagen" 
+            onChange={actualizarArchivoState}
+          />
         </div>
 
         <div className="enviar">
