ff8b2a1292decfc78567492f2f2077da301000d7
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-02 23:57:34 -0400

    Almacenando los resultados de la consulta en el state y useState

--- 76-nodereact_CreandoApp/client-api/src/components/clientes/Clientes.js
+++ 76-nodereact_CreandoApp/client-api/src/components/clientes/Clientes.js
@@ -1,14 +1,19 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 // ClienteAxios
 import clienteAxios from '../../config/exios';
 
 const Clientes = () => {
+    // Hook useState - clientes: state y guardarCliente: funcion para guardar el state
+    const [clientes, guardarClientes] = useState([]);
+
     // Query a la API
     const consultarAPI = async () => {
         // console.log('Consultado...');
 
-        const clientes = await clienteAxios.get('/clientes');
-        console.log(clientes);
+        const clientesConsulta = await clienteAxios.get('/clientes');
+        // console.log(clientesConsulta.data);
+
+        guardarClientes(clientesConsulta.data);
     };
 
     // Hook useEfect, similar a componetdidmount y willmount
@@ -16,8 +21,17 @@ const Clientes = () => {
         consultarAPI();
     }, []);             // [] - Ejecuta solo una vez
 
+
     return (
-        <h2>Clientes</h2>
+        <Fragment>
+            <h2>Clientes</h2>
+
+            <ul className='listado-clientes'>
+                {clientes.map(cliente => {
+                    console.log(cliente);
+                })}
+            </ul>
+        </Fragment>
     );
 };
 
