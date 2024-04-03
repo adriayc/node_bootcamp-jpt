3d35284684f172fdb987541978cb792b6e7cd406
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-03 16:50:26 -0400

    Eliminar Clientes

--- 76-nodereact_CreandoApp/client-api/src/components/clientes/Cliente.js
+++ 76-nodereact_CreandoApp/client-api/src/components/clientes/Cliente.js
@@ -1,9 +1,44 @@
 import { Link } from "react-router-dom";
+import Swal from 'sweetalert2';
+// ClienteAxios
+import clienteAxios from '../../config/axios';
 
 const Cliente = ({cliente}) => {
     // console.log(cliente);
     // Destructuring del cliente
     const { _id, nombre, apellido, empresa, email, telefono } = cliente;  
+
+    // Eliminar un cliente de la API REST (Backend)
+    const eliminarCliente = id => {
+        // console.log('Eliminando...');
+        // console.log(id);
+
+        Swal.fire({
+            title: "¿Estas seguro de eliminar?",
+            text: "¡El cliente eliminado no puede recurerar!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "¡Si eliminarlo!",
+            cancelButtonText: "Cancelar"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Enviar la petición por el clienteAxios
+                clienteAxios.delete(`/clientes/${id}`)
+                    .then(res => {
+                        // console.log(res);
+
+                        // Mostrar alerta
+                        Swal.fire({
+                            title: "¡Eliminado!",
+                            text: res.data.mensaje,
+                            icon: "success"
+                        });
+                    });
+            }
+          });
+    }
 
     return (
       <li className="cliente">
@@ -18,7 +53,12 @@ const Cliente = ({cliente}) => {
                 <i className="fas fa-pen-alt"></i>
                 Editar Cliente
             </Link>
-            <button type="button" className="btn btn-rojo btn-eliminar">
+            <button 
+                type="button" 
+                className="btn btn-rojo btn-eliminar"
+                // Se ejecuta la fn cuando ocurre un evento
+                onClick={() => eliminarCliente(_id)}
+            >
                 <i className="fas fa-times"></i>
                 Eliminar Cliente
             </button>
--- 76-nodereact_CreandoApp/client-api/src/components/clientes/Clientes.js
+++ 76-nodereact_CreandoApp/client-api/src/components/clientes/Clientes.js
@@ -9,20 +9,31 @@ const Clientes = () => {
     // Hook useState - clientes: state y guardarCliente: funcion para guardar el state
     const [clientes, guardarClientes] = useState([]);
 
-    // Query a la API
-    const consultarAPI = async () => {
-        // console.log('Consultado...');
+    // // Query a la API
+    // const consultarAPI = async () => {
+    //     // console.log('Consultado...');
 
-        const clientesConsulta = await clienteAxios.get('/clientes');
-        // console.log(clientesConsulta.data);
+    //     const clientesConsulta = await clienteAxios.get('/clientes');
+    //     // console.log(clientesConsulta.data);
 
-        guardarClientes(clientesConsulta.data);
-    };
+    //     guardarClientes(clientesConsulta.data);
+    // };
 
     // Hook useEfect, similar a componetdidmount y willmount
     useEffect(() => {
+        // Query a la API
+        const consultarAPI = async () => {
+            // console.log('Consultado...');
+
+            const clientesConsulta = await clienteAxios.get('/clientes');
+            // console.log(clientesConsulta.data);
+
+            guardarClientes(clientesConsulta.data);
+        };
+
         consultarAPI();
-    }, []);             // [] - Ejecuta solo una vez
+    // }, []);                     // [] - Ejecuta solo una vez
+    }, [clientes]);             // [clientes] - Ejecuta cada vez que clientes cambia
 
 
     return (
