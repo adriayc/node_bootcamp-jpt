64126d64ebef20405e9ca991bf7eb449ddaf2cc2
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-05 18:27:00 -0400

    Autenticando usuario y generar JWT (Backend)

--- 69-RestApis_CreandoProyectoRestApis/restapis/controllers/usuarioController.js
+++ 69-RestApis_CreandoProyectoRestApis/restapis/controllers/usuarioController.js
@@ -1,4 +1,5 @@
-const bscrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 // Models
 const Usuario = require('../models/Usuario');
 
@@ -24,6 +25,46 @@ exports.registrarUsuario = async (req, res) => {
 };
 
 // Autenticar un usuario
-exports.autenticarUsuario = () => {
+exports.autenticarUsuario = async (req, res, next) => {
+    // Obtener el email
+    const { email, password } = req.body;
+
+    try {
+      // Obtener un usuario
+      const usuario = await Usuario.findOne({email});
+
+      // Validar el usuario
+      if (!usuario) {
+        // Devolver una respuesta
+        res.status(401).json({'mensaje': 'El usuario no exite'});
+        return next();
+      }
+
+      // Validar el password
+      if (!bcrypt.compareSync(password, usuario.password)) {
+        // Devolver una respuesta
+        res.status(401).json({'mensaje': 'El password es incorrecto'});
+        return next();
+      }
+
+      // Crear un token con JWT
+      const token = jwt.sign({
+        email: usuario.email,
+        nombre: usuario.nombre,
+        _id: usuario._id
+      },
+      // Llave secreta
+      'LLAVESECRETA',
+      {
+        // Expiracion del token
+        expiresIn: '1h'
+      });
+
+      res.json({token});
+
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
 
 };
