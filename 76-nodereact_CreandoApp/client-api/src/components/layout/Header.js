60e4a7efa2c6e7f767f08d31af01a63aa995fae8
Adriano Ayala <adriano.ayc@gmail.com>
2024-04-06 12:28:32 -0400

    Cerrar Sesion y Ocultar partes de la Interfaz si el usuario no esta autenticado

--- 76-nodereact_CreandoApp/client-api/src/components/layout/Header.js
+++ 76-nodereact_CreandoApp/client-api/src/components/layout/Header.js
@@ -1,3 +1,8 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+// Contexts
+import CrmContext from "../../context/CrmContent";
+
 // Component
 // const Header = () => {
 //   return (
@@ -10,12 +15,49 @@
 // };
 
 // Component
-const Header = () => (
-  <header className="barra">
-    <div className="contenedor">
-      <h1>CRM - Administrador de Clientes</h1>
-    </div>
-  </header>
-);
+const Header = () => {
+  // Definir el context CrmContext
+  const { auth, guardarAuth } = useContext(CrmContext);
+
+  const navigate = useNavigate();
+
+  // Cerrar sesión
+  const cerrarSesion = () => {
+    // console.log('Cerrando sesión...');
+
+    // Actualizar el state del context
+    guardarAuth({
+      token: '',
+      auth: false
+    });
+
+    // Eliminar token del LocalStorage
+    localStorage.removeItem('token');
+
+    // Redireccionar
+    navigate('/iniciar-sesion')
+  };
+
+  return (
+    <header className="barra">
+      <div className="contenedor">
+        <div className="contenido-barra">
+          <h1>CRM - Administrador de Clientes</h1>
+
+          {auth.auth ? (
+            <button 
+              type="button" 
+              className="btn btn-rojo"
+              onClick={cerrarSesion}
+            >
+              <i className="far fa-times-circle"></i>
+              Cerrar Sesión
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
